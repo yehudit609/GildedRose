f4841dc712fc103472f23eec0565b3721f8dcb50
yehudit609 <37326058609@mby.co.il>
2024-10-07 20:54:28 +0300

    add backstage passes updater

--- src/BackstagePassesItemUpdater.php
+++ src/BackstagePassesItemUpdater.php
@@ -7,7 +7,19 @@ class BackstagePassesItemUpdater implements IItemUpdater
 {
     public function update(Item $item): void
     {
-        
-        
-    }
+        if ($item->sellIn < 1) {
+            $item->quality = 0;
+        } elseif ($item->sellIn < 6) {
+            $item->quality += 3;
+        } elseif ($item->sellIn < 11) {
+            $item->quality += 2;
+        } else {
+            $item->quality++;
+        }    
+
+        if($item->quality > 50) {
+            $item->quality = 50;
+        }
+}
+    
 }
