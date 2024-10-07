4a9b8d146b4286de81c31f5c6280cf45f45a096d
yehudit609 <37326058609@mby.co.il>
2024-10-07 17:57:01 +0300

    add sulfuras updater

--- src/SulfurasItemUpdater.php
+++ src/SulfurasItemUpdater.php
@@ -5,9 +5,11 @@ namespace GildedRose;
 
 class SulfurasItemUpdater implements IItemUpdater
 {
+    //The function is based on the requirement:
+    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+
     public function update(Item $item): void
     {
-        
-        
+        $item->sellIn++;        
     }
 }
