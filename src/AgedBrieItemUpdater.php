d28d3c8c906a6ca702dfe97835a5622cedd49c89
yehudit609 <37326058609@mby.co.il>
2024-10-07 21:35:47 +0300

    add AgedBrie updater

--- src/AgedBrieItemUpdater.php
+++ src/AgedBrieItemUpdater.php
@@ -7,7 +7,17 @@ class AgedBrieItemUpdater implements IItemUpdater
 {
     public function update(Item $item): void
     {
-        
+        $this->increaseQuality($item);
+        if ($item->sellIn < 0) {
+            $this->increaseQuality($item);
+        }
+    }
+
+    private function increaseQuality(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality++;
+        }
         
     }
 }
